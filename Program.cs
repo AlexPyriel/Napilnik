e76a5a9693ac02db590c96d7b2120fa19db7474d
AlexPyriel <alexpyriel@yandex.ru>
2022-07-04 22:28:22 +0300

    # Decomposition

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Player
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+}
 
+class Movement
+{
+    public float DirectionX { get; private set; }
+    public float DirectionY { get; private set; }
+    public float Speed { get; private set; }
+
+    public void Move()
+    {
+        //Do move
+    }
+}
+
+class Weapon
+{
+    public float Cooldown { get; private set; }
+    public int Damage { get; private set; }
+    public bool IsReloading()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Attack()
+    {
+        //attack
+    }
+}
